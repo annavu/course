62f1f356b3ccc1a7271c3e5dfbdfd856c09288a5
annawrzos <anna.wrzos@monogo.pl>
2019-06-17 08:54:25 +0200

    update Spinner component

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,13 +1,9 @@
 import React, { Fragment } from 'react';
 import spinner from './spinner.gif';
 
-const Spinner = props => {
-    return (
-        <Fragment>
-            <img src={spinner} alt="Loading..."/>
+const Spinner = () => <Fragment>
+            <img src={spinner} alt="Loading..." style={{width:'200px', margin:'auto', display:'block'}}/>
         </Fragment>
-    )
-}
 
-
+//if we don't have any js we can return this directly - without return function
 export default Spinner;
